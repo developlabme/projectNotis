fba0e06f8b3639c51f16d95331a9c3f2e2fe3289
Oscar Marq Olv <oscar.marq.olv@gmail.com>
2023-02-25 11:42:33 -0600

    Changes to be committed: 	modified:   routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/new', function(req, res, next) {
+  ressend('creando un nuevo usuario...');
+});
+
 module.exports = router;
