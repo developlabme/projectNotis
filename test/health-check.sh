7cdd9bfa05638c7c1c7d49d41e6d2cc09dc50a2e
Oscar Marquez Olv <oscar.marq.olv@gmail.com>
2023-02-25 11:52:03 -0600

    CodeReview: DEVOP-6  API User (#4)
    * Iniciando desarrollo de API-User
    
    DVOP-6-ApiUser #iniciando #time 15M #comment Se crearan los metodos POST y GET
    
     Changes to be committed:
    	modified:   Changelog.md
    
    * Changes:
     - Creando archivo de pruebas `./test/health-check.sh`
     - Creando directorio `./storage/db-test` con un respaldo de DB para pruebas
    
     DVOP-6-ApiUser #time 3h #comment Creando archivos faltantes para pruebas
    
     Changes to be committed:
    	new file:   storage/db-test/backup.txt
    	new file:   test/health-check.sh
    
    * Changes to be committed:
    	modified:   routes/users.js
    
    * Changes to be committed:
    	modified:   test/health-check.sh
    
    * Bugfix...

---
+++ test/health-check.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -
+
+#
+# UNIT-TEST
+#
+
+
+echo "Iniciando prruebas Unitarias...";
+sleep 10;
+
+echo "La idea de este script es apilar las distintas pruebas historicas que validan los cambios en la aplicacion";
+sleep 10;
+
+echo "Lanzando las primeras pruebas...";
+sleep 10;
